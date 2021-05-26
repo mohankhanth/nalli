75615833c2fe4608a14b7781caa2a2b3854805df
mohankhanth <mohankhanth@gmail.com>
2021-05-27 00:03:01 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { NewArrivalsComponent } from './components/new-arrivals/new-arrivals.component';
+import { RecentlyViewdComponent } from './components/recently-viewd/recently-viewd.component';
+import { SimilarProductsComponent } from './components/similar-products/similar-products.component';
+import { TrendingComponent } from './components/trending/trending.component';
+import { BannerComponent } from './components/banner/banner.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewArrivalsComponent,
+    RecentlyViewdComponent,
+    SimilarProductsComponent,
+    TrendingComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
